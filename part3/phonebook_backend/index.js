ca6cc23035bff738fff91c752e0209bfe58df5b5
Vedat Tavas <vtavas@hotmail.com>
2020-07-19 10:33:30 +0300

    Ex 3.3 added

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -43,6 +43,16 @@ let persons = [
     response.send("<div>Phonebook has info for " + arrLen + " people</div>"
      +"<br>"+"<div>" + date + "</div>")
   })
+
+  app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    if (person) {
+      response.json(person)
+    } else {
+      response.status(404).end()
+    }
+  })
   
   const PORT = 3001
   app.listen(PORT, () => {
