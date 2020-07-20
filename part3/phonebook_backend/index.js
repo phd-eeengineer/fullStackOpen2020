f13afff213fac904b2b8c4876d7d35f2e031c6f4
Vedat Tavas <vtavas@hotmail.com>
2020-07-20 21:31:59 +0300

    Ex 3.8 morgan - consol log

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -3,7 +3,16 @@ var morgan = require('morgan')
 
 const app = express()
 app.use(express.json())
-app.use(morgan('tiny'))
+//app.use(morgan('tiny'))
+morgan.token('data', function (req, res) { 
+  if (req.method === "POST") {
+    return JSON.stringify(req.body)
+ } else {
+  return null
+ }
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 
 let persons = [
     {
