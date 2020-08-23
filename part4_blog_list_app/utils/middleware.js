2fa81f89e523e02ebeba7f4ae8274074b7783754
Vedat Tavas <vtavas@hotmail.com>
2020-08-23 15:04:52 +0300

    Ex 4.20 middleware

--- part4_blog_list_app/utils/middleware.js
+++ part4_blog_list_app/utils/middleware.js
@@ -12,6 +12,15 @@ const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
 
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get('authorization')  
+    
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {    
+    request.token = authorization.substring(7)  
+  }  
+  next()
+}
+
 const errorHandler = (error, request, response, next) => {
   logger.error(error.message)
 
@@ -24,12 +33,12 @@ const errorHandler = (error, request, response, next) => {
       error: error.message
     })
   }
-
   next(error)
 }
 
 module.exports = {
   requestLogger,
+  tokenExtractor,
   unknownEndpoint,
   errorHandler
 }
